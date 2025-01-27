58fabcf1a6b08ea902c899bc9da85f639a8b27da
unknown <flavio.santos.ti@outlook.com>
2025-01-27 14:53:25 -0300

    ✨ feat: create reusable profile dropdown component

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileDropdownComponent } from '../profile-dropdown/profile-dropdown.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProfileDropdownComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  isDropdownOpen = false;
-
-  toggleDropdown(): void {
-    this.isDropdownOpen = !this.isDropdownOpen;
+ 
+  handleProfileClick(): void {
+    console.log('Profile clicked');
+    // Implementar lógica adicional, se necessário
   }
-  
-  closeDropdown(): void {
-    this.isDropdownOpen = false;
-  }  
+
+  handleSignOutClick(): void {
+    console.log('Sign out clicked');
+    // Implementar lógica de logout ou navegação
+  }
+ 
 }
