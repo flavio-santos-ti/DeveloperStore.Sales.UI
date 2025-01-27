2c3413281e34e45e94d330cda877ff725fd4937a
unknown <flavio.santos.ti@outlook.com>
2025-01-27 09:26:57 -0300

    ✨ feat: Implement home page UI with Angular, Tailwind, and FontAwesome icons.

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -16,16 +17,19 @@ export class LoginComponent {
   password: string = '';
   passwordVisible: boolean = false; // This variable is used to toggle the visibility of the password input field
 
-  constructor(library: FaIconLibrary) {
+  constructor(private router: Router, library: FaIconLibrary) {
     library.addIcons(faEye, faEyeSlash);
   }
 
   togglePasswordVisibility(): void {
     this.passwordVisible = !this.passwordVisible;
   }
-
+  
   onSubmit(): void {
     console.log('Username:', this.username);
     console.log('Password:', this.password);
+
+    // Here you can implement any validation logic, but for now, the redirection will be don unconditionally.
+    this.router.navigate(['/home']);
   }
 }
