5b22a63e1bd5cab54169ded775a16c122d3cc4f7
unknown <flavio.santos.ti@outlook.com>
2025-01-28 11:27:26 -0300

    ♻️ refactor: split page components into separate modules

--- angular/src/app/pages/home/home.component.ts
+++ angular/src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProfileDropdownComponent } from '../profile-dropdown/profile-dropdown.component';
-import { SearchComponent } from '../search/search.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ProfileDropdownComponent } from '../../components/profile-dropdown/profile-dropdown.component';
+import { SearchComponent } from '../../components/search/search.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
@@ -27,6 +27,4 @@ export class HomeComponent {
     console.log('Search value:', searchValue);
     // Implement logic to handle the search value
   }
- 
- 
 }
