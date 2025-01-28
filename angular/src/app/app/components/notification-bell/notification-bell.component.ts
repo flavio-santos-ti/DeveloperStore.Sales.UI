aa6bd3541dd34174a063dc238a697be6fc329352
unknown <flavio.santos.ti@outlook.com>
2025-01-28 12:27:40 -0300

    ✨ feat: add NotificationBellComponent

---
+++ angular/src/app/app/components/notification-bell/notification-bell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-bell',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-bell.component.html',
+  styleUrl: './notification-bell.component.css'
+})
+export class NotificationBellComponent {
+
+}
